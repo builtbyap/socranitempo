cb9196c185ec7101ec6b43e5af923ef01d9fb4e3
builtbyap <abongmabo345@gmail.com>
2025-06-08 05:34:45 -0400

    Add Firebase auth and Firestore configuration files

--- src/lib/firebase/db.ts
+++ src/lib/firebase/db.ts
@@ -10,5 +10,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); 
