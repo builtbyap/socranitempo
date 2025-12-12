455128238142b39e89724e8223b713206a5e058a
Abong Mabo <thesocrani@gmail.com>
2025-12-12 14:23:36 -0500

    Initial Commit

---
+++ surgeapp/surgeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  surgeappApp.swift
+//  surgeapp
+//
+//  Created by Abong Mabo on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct surgeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
